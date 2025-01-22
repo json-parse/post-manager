27d01fa39f360d0cb2f27e24eea234520943f601
Maite <o_o.maite@hotmail.com>
2025-01-22 16:45:49 +0100

    update app title to 'Post manager' and remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import reportWebVitals from "./reportWebVitals.ts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
